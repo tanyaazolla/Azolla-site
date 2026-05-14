6ff897f28fd2e2ee099fd0ada6039bc89036546a
tanya <tanya.m@azolla.sg>
2026-05-14 16:31:18 +0530

    alignment issue fixed

--- src/_pages/AzollaEmpowers.tsx
+++ src/_pages/AzollaEmpowers.tsx
@@ -3,8 +3,9 @@ import useBreakpoints from '@/hooks/useBreakPoints';
 
 const colorChangeText = [
   'Azolla enables customers to move beyond compliance to performance,',
-  ' delivering decarbonization solutions that reduce cost,',
-  'improve efficiency, and enhance asset value.',
+  ' delivering decarbonization solutions that reduce cost, ',
+  'improve efficiency, and enhance asset value.    ',
+  <br />,
   ' Backed by Synergy Group, it combines advisory, execution,',
   ' and digital capabilities to deliver measurable impact at scale.',
 ];
@@ -12,7 +13,8 @@ const colorChangeText = [
 const textForMobile = [
   'Azolla enables customers to move beyond compliance to performance,',
   'delivering decarbonization solutions that reduce cost,',
-  'improve efficiency, and enhance asset value.',
+  'improve efficiency, and enhance asset value.', 
+
   'Backed by Synergy Group, it combines advisory, execution,',
   ' and digital capabilities to deliver measurable impact at scale.',
   
@@ -20,11 +22,11 @@ const textForMobile = [
 
 function AzollaEmpowers() {
   const { isXs } = useBreakpoints();
-  return (
-   <div className='p-6 md:p-10 xl:p-20 text-responsive-4xl lg:text-[2.5vw] !leading-[1.2] text-white font-[500] bg-black'>
-      {isXs ? textForMobile : colorChangeText}
-    </div>
-  );
+ return (
+  <div className='p-6 md:p-10 xl:p-20 text-left text-responsive-4xl lg:text-[3vw] !leading-[1.2] text-white font-[500] bg-black'>
+    {isXs ? textForMobile : colorChangeText}
+  </div>
+);
 }
 
 export default AzollaEmpowers;
